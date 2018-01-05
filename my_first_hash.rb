b807e1af05a87bcf3e5789973e490cd59c59c327
Christina Collura <cmcollura@gmail.com>
2018-01-05 16:47:35 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,20 +2,18 @@ def my_hash
   my_hash = {
     "name" => "Christina",
     "age" => 35,
-    "hair_color" => "brown",
+    "hair color" => "brown",
     "personality" => "quirky"
   }
-
-  
 end
 
 
 def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information: 
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+  }
 end
 
 def retrieval
