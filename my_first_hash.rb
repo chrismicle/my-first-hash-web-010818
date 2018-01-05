f2090408d890895f47abde125d69e662b97e0c83
Christina Collura <cmcollura@gmail.com>
2018-01-05 17:02:43 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "pearl necklace" => 1}
   shipping_manifest["muskets"] = 2 
   shipping_manifest["gun powder"] = 4
-  return shipping_manifest
+  puts shipping_manifest
 end
 
 
